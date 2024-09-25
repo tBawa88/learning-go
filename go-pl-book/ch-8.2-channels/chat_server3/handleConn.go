64a7030ff13f17d2713d7bb7e9c09773db9d7dcc
tbawa88 <tbawa3388@gmail.com>
2024-09-25 22:41:37 +0530

    exercise - chat server

--- go-pl-book/ch-8.2-channels/chat_server3/handleConn.go
+++ go-pl-book/ch-8.2-channels/chat_server3/handleConn.go
@@ -1,7 +1,46 @@
 package main
 
-import "net"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"time"
+)
+
+const timemout = time.Second * 5
 
 func handleConn(conn net.Conn) {
+	defer conn.Close()
+	clientChan := make(chan string)
+	go clientWriter(conn, clientChan)
 
+	who := conn.RemoteAddr().String()
+	clientChan <- "You are : " + who
+	messages <- who + " has joined the chat"
+	entering <- clientChan
+
+	// start the timer
+	timer := time.NewTimer(timemout)
+	go func() {
+		<-timer.C
+		conn.Close()
+	}()
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		text := input.Text()
+		messages <- who + " : " + text
+		timer.Reset(timemout) // just reset the same timer with same timeout
+	}
+
+	//wait for either connectoin to close, or client getting kicked due to afk
+	leaving <- clientChan
+	messages <- who + " has left the chat"
+
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg)
+	}
 }
